5be6cf85442d072897d6693a14f5998ddafa10bb
Axebow2 <149180749+Axebow2@users.noreply.github.com>
2025-04-11 15:15:37 +0100

    Secondary initial Commit

--- app/src/main/java/com/example/notetakingapp/MainActivity.kt
+++ app/src/main/java/com/example/notetakingapp/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "What the freak is up $name!",
         modifier = modifier
     )
 }
